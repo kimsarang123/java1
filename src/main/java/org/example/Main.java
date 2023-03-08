7035b8c26335142311ced194f3a65b58abfe9bb6
kimsarang123 <02ove@nave.com>
2023-03-08 12:25:56 +0900

    123

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // 변수
+        int age = 19;
+        System.out.println("당신의 나이 : "+ age);
+
+
+
+        if ((age >= 20 &&) (age <= 34)) {
+            System.out.println("청년입니다.");
+        }else {
+            System.out.println("청년이 아닙니다.");
+        }
     }
 }
